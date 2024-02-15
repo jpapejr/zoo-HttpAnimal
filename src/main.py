1b3c68494fef7b2a47a0652df97520fe7b2e4028
jpapejr <jpapejr@icloud.com>
2024-02-15 13:40:12 -0500

    refactor: black

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ import random
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello():
     return ("(animals can't talk, right?)", 200)
@@ -11,8 +12,9 @@ def hello():
 
 @app.route('/exhibit')
 def ex():
-    if os.getenv('IMGURL') is not None: 
-        return ('<img src="' + os.getenv('IMGURL') + '" height="25%" width="25%" />', 200)
+    if os.getenv('IMGURL') is not None:
+        return ('<img src="' + os.getenv('IMGURL') +
+                '" height="25%" width="25%" />', 200)
     else:
         return ('<img src="https://zoologyfoundation.org/wp-content/uploads/woocommerce-placeholder.png" height="25%" width="25%" />', 200)
 
@@ -24,11 +26,11 @@ def action():
     else:
         name = 'animal'
 
-    if os.getenv('ACTIONS') is not None: 
+    if os.getenv('ACTIONS') is not None:
         actionList = os.getenv('ACTIONS').split(',')
         random.seed()
-        selection = random.randint(0, len(actionList) -1)
+        selection = random.randint(0, len(actionList)-1)
     else:
         return ('The animal is not visible right now.', 200)
-        
     return ('The ' + name + ' ' + actionList[selection], 200)
+
